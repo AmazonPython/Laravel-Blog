e6840dd032a9fcd9802f82c514ac70984f511bd6
AmazonPython <amazon3@foxmail.com>
2020-08-10 22:58:23 +0800

    Laravel-Blog v1.1.7 Using composer mirror address from aliyun, and updated the title field in create_articles_table

---
+++ database/migrations/2020_08_10_222830_change_articles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            //运行php artisan make:migration change_articles_table --table=articles
+            //在新迁移表中使用change->()命令修改字段长度为60
+            $table->string('title', 60)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            //
+        });
+    }
+}
